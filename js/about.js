3c6da282ac8c91c2a116647303023693f04f7abc
Nikita <gomzyakoff.nickita@yandex.ru>
2026-09-03 17:44:21 +0300

    Добавил maps в раздел About

--- js/about.js
+++ js/about.js
@@ -1,5 +1,25 @@
-// Выезжающие изображения друг из под друга
+//Скролл хедер блока
+document.body.style.overflow = "hidden"; // Изначально блокируем скролл header
+setTimeout(() => {
+  document.body.style.overflow = "";
+}, 1000); //Разрешаем скроллить через 1 сек
 
+let lastedScroll = window.scrollY;
+const headerTop = document.querySelector(".header__top");
+
+if (headerTop) {
+  window.addEventListener("scroll", () => {
+    const currentScroll = window.scrollY;
+    if (currentScroll > lastedScroll) {
+      headerTop.classList.add("hidden");
+    } else {
+      headerTop.classList.remove("hidden");
+    }
+    lastedScroll = currentScroll;
+  });
+}
+
+// Выезжающие изображения друг из под друга
 const teamAnimation = document.querySelector(".team__animation-wrapper");
 const images = gsap.utils.toArray(".team__animation-wrapper img");
 
@@ -13,9 +33,15 @@ let tl = gsap.timeline({
   },
 });
 
-tl.fromTo(images[1],
+tl.fromTo(
+  images[1],
   { rotateY: 25, x: 0, opacity: 0 },
-  { rotateY: 0, x: -450, opacity: 1 }, 0);
-tl.fromTo(images[2],
+  { rotateY: 0, x: -450, opacity: 1 },
+  0,
+);
+tl.fromTo(
+  images[2],
   { rotateY: -25, x: 0, opacity: 0 },
-  { rotateY: 0, x: 450, opacity: 1 }, 0); 
+  { rotateY: 0, x: 450, opacity: 1 },
+  0,
+);
